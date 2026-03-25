b975be82700f041f4e688c91b220174d202b6b9e
Shreedhar Halli <mayurbusinesssolutions@gmail.com>
2026-03-25 18:29:10 +0530

    Restore mandatory --no-sandbox flag

--- app.js
+++ app.js
@@ -174,6 +174,9 @@ function whatsappFactoryFunction(clientId) {
       ...(process.env.PUPPETEER_EXECUTABLE_PATH && { executablePath: process.env.PUPPETEER_EXECUTABLE_PATH }),
       headless: true,
       args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
         '--disable-gpu'
       ],
     },
